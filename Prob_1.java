c6d6e13e0b9e9d6bc11c5ce7006566035cfeb299
almanzar-j <113154922+almanzar-j@users.noreply.github.com>
2022-12-28 00:16:10 -0500

    Add files via upload

---
+++ Prob_1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Prob_1 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for(int i = 1; i < 1000; i++){
+            if((i % 3 == 0) && (i % 5 == 0)){
+                sum += i;
+            }else if (i % 3 == 0){
+                sum += i;
+            }else if(i % 5 == 0){
+                sum += i;
+            }
+
+        }
+
+        System.out.println(sum);
+    }
+
+}
